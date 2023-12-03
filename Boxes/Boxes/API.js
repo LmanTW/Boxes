b57694341c7c475a418ecd1927d7512c67fc313b
LmanTW <82919705+LmanTW@users.noreply.github.com>
2023-12-03 17:56:19 +0800

    Delete Boxes directory

--- Boxes/Boxes/API.js
+++
@@ -1,9 +0,0 @@
-export {
-  Actuator,
-  Compiler,
-  VirtualMachine
-}
-
-import VirtualMachine from './Modules/VirtualMachine/Main.js'
-import Compiler from './Modules/Compiler/Main.js'
-import Actuator from './Modules/Actuator.js'
--- Boxes/index.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs'
-
-import { Actuator } from './Boxes/API.js'
-
-let actuator = new Actuator()
-console.log(actuator.run(fs.readFileSync('/Users/lukelin/Desktop/Boxes/test.boxes', 'utf8')))
-
-//console.log(Compiler.compile(fs.readFileSync('/Users/lukelin/Desktop/Boxes/test.boxes', 'utf8')))
