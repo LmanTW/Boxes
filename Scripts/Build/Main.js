cd605a58fc3ced08033d7f21bdf735c682bf876b
LmanTW <82919705+LmanTW@users.noreply.github.com>
2023-12-11 21:39:06 +0800

    Change file structure

--- Scripts/Build/Main.js
+++ Scripts/Build/Main.js
@@ -2,12 +2,12 @@ const tsup = require('tsup')
 const path = require('path')
 
 tsup.build({
-  entry: [path.resolve(__dirname, '../BoxesEngine/API.js')],
+  entry: [path.resolve(__dirname, '../../BoxesEngine/API.js')],
 
   format: 'esm',
   minify: true,
 
-  outDir: path.resolve(__dirname, '../'),
+  outDir: path.resolve(__dirname, '../../Assets/'),
 
   globalName: 'BoxesEngine'
 })
--- Scripts/Test/index.js
+++ Scripts/Test/index.js
@@ -2,7 +2,7 @@ import { fileURLToPath } from 'url'
 import { dirname } from 'path'
 import fs from 'fs'
 
-import { Compiler, VirtualMachine } from '../BoxesEngine/API.js'
+import { Compiler, VirtualMachine } from '../../BoxesEngine/API.js'
 
 (async () => {
   let data = Compiler.compile(fs.readFileSync(`${dirname(fileURLToPath(import.meta.url))}/test.boxes`, 'utf8'))
